161d5054106637cd2c4a550f48e792395d37f40b
AlHasan <playhasanv@gmail.com>
2021-06-24 09:33:33 +0300

    structure update: day four.

--- src/data_structure/day_four/Q7.java
+++ src/data_structure/day_four/Q7.java
@@ -1,22 +1,36 @@
 package data_structure.day_four;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Q7 {
     public static void sortTwoArrays(Integer[] a, Integer[] b) {
-        int nonEmptyInA = 0;
+        int aNulls = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] == null) {
-                nonEmptyInA = a.length - i;
+                aNulls = a.length - i;
                 break;
             }
         }
-        System.out.println(nonEmptyInA);
+        int i = aNulls - 1;
+        int j = b.length - 1;
+        int lastIndex = aNulls + b.length - 1;
+        while (j >= 0) {
+            if (i >= 0 && a[i] > b[j]) {
+                a[lastIndex] = a[i];
+                i--;
+            } else {
+                a[lastIndex] = b[j];
+                j--;
+            }
+            lastIndex--;
+        }
     }
 
     public static void main(String[] args) {
-        Integer[] a = new Integer[]{1,2,3,null,null,null};
-        Integer[] b = new Integer[]{1,2,3};
-        sortTwoArrays(a,b);
+        Integer[] a = new Integer[]{1, 2, 3, null, null, null};
+        Integer[] b = new Integer[]{1, 2, 3};
+        sortTwoArrays(a, b);
+
+        System.out.println(Arrays.toString(a));
     }
 }
