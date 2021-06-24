87968edc334d4c5661eb46af55780a80b1ed8b0f
AlHasan <playhasanv@gmail.com>
2021-06-24 10:32:44 +0300

    data structure update: day four.

--- src/data_structure/day_four/Q5.java
+++ src/data_structure/day_four/Q5.java
@@ -1,28 +1,33 @@
 package data_structure.day_four;
 
 public class Q5 {
-    public static Q4.Node<Integer> nthToLast(Q4.Node<Integer> head, int n) { //last here is the tail NULL
-        if (head == null || n < 1) {
+
+    public static Q4.Node<Integer> nthToLast(Q4.Node<Integer> head, int n) {
+
+        Q4.Node<Integer> firstNode = head;
+        Q4.Node<Integer> secondNode = head;
+
+        if (n == 0) {
             return null;
         }
-        Q4.Node<Integer> p1 = head;
-        Q4.Node<Integer> p2 = head;
 
-        for (int j = 0; j < n - 1; ++j) {
-            if (p2 == null) {
+        for (int i = 0; i < n; i++) {
+            if (secondNode != null) {
+                secondNode = secondNode.next;
+            } else {
                 return null;
             }
-            p2 = p2.next;
-        }
-        while (p2.next != null) {
-            p1 = p1.next;
-            p2 = p2.next;
         }
 
-        return p1;
+        while (secondNode != null) {
+            firstNode = firstNode.next;
+            secondNode = secondNode.next;
+        }
+        return firstNode;
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) { //last here is the tail NULL
         Q4.Node<Integer> tail = new Q4.Node<>(null, 2);
         Q4.Node<Integer> n4 = new Q4.Node<>(tail, 4);
         Q4.Node<Integer> n3 = new Q4.Node<>(n4, 11);
@@ -30,6 +35,6 @@ public class Q5 {
         Q4.Node<Integer> head = new Q4.Node<>(n2, 8);
 
         Q4.printNodes(head);
-        System.out.println(nthToLast(head,0).data);
+        System.out.println(nthToLast(head, 4).data);
     }
 }
