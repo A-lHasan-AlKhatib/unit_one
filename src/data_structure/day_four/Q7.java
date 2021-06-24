461f095e9ffdc8a4b0d8448fffbff144ba2f7d2b
AlHasan <playhasanv@gmail.com>
2021-06-24 12:50:22 +0300

    data structure update: day four

--- src/data_structure/day_four/Q7.java
+++ src/data_structure/day_four/Q7.java
@@ -3,28 +3,34 @@ package data_structure.day_four;
 import java.util.Arrays;
 
 public class Q7 {
+
     public static void sortTwoArrays(Integer[] a, Integer[] b) {
-        int aNulls = 0;
+        int lastA = 0;
+        int lastB = b.length;
         for (int i = 0; i < a.length; i++) {
             if (a[i] == null) {
-                aNulls = a.length - i;
+                lastA = a.length - i;
                 break;
             }
         }
-        int i = aNulls - 1;
-        int j = b.length - 1;
-        int lastIndex = aNulls + b.length - 1;
-        while (j >= 0) {
-            if (i >= 0 && a[i] > b[j]) {
-                a[lastIndex] = a[i];
-                i--;
+
+        int indexMerged = lastB + lastA - 1; // Index of last location of merged array
+        int indexA = lastA - 1;
+        int indexB = lastB - 1;
+
+        //start form the end of the arrays
+        while (indexB >= 0) {
+            if (indexA >= 0 && a[indexA] >= b[indexB]) {
+                a[indexMerged] = a[indexA];
+                indexA--;
             } else {
-                a[lastIndex] = b[j];
-                j--;
+                a[indexMerged] = b[indexB];
+                indexB--;
             }
-            lastIndex--;
+            indexMerged--;
         }
     }
+
 
     public static void main(String[] args) {
         Integer[] a = new Integer[]{1, 2, 3, null, null, null};
